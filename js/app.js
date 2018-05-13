a230f93f2f424ce193c732c37c9e3e7f36fa140a
Hannybaby <hannybaby@gmail.com>
2018-05-13 21:49:53 +0200

    Final changes

--- js/app.js
+++ js/app.js
@@ -1,130 +1,101 @@
-// Enemies our player must avoid 
-var InGameObjects = function (x, y) {
-    this.start = 0;
-    this.end = 500; 
-    this.x = 0;
-    this.y = 0;
-    this.sprite;
-}
+// Draw objects on screen
+Object.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+    // When on top reset player
+    if (this.y <= 0){ 
+    alert("⛥ Congrats! You won! ⛥ Click OK to play again!" );    
+    this.reset();
+    }
+};
+
+//Player start position
+Object.prototype.reset = function() {
+  player.x = 200;
+  player.y = 385;
+};
 
 
-
-
+// Enemies the player must avoid
 var allEnemies = [];
+var Enemy = function(x,y) {
 
-var Enemy = function(x) {
-    InGameObjects.call(this, Enemy);
     
     this.sprite = 'images/enemy-bug.png';
-    this.width = 52;
-    this.height = 55; 
 
-};
-
-Enemy.prototype = Object.create(InGameObjects.prototype);
-Enemy.prototype.constructor = Enemy;
-
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    
-
-    
-    for (var i = 0; i < allEnemies.length; i++) {
-        allEnemies[i].start = (Math.floor(Math.random() * (-2000)) + 2);
-        // bugs speed
-        allEnemies[i].x += (50 * dt); 
-        //allEnemies[i].y = allEnemies[i].y + 80;
-
-         this.checkCollisions();
-
-        //stay inside map (y)
-        if (allEnemies[i].y > 220) {
-            allEnemies[i].y = 60;
-        }
-
-        //stay inside map (x)
-        if (allEnemies[i].x >= this.end) {
-            allEnemies[i].x = allEnemies[i].start;
-        }
-    }
-    
-};
-
-    
-
-
-// Draw the enemy on the screen
-Enemy.prototype.render = function() {
-
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-Enemy.prototype.checkCollisions = function() {
-    if (player.x < this.x + this.width &&
-   player.x + player.width > this.x &&
-   player.y < this.y + this.height &&
-   player.height + player.y > this.y) {
-    // collision detected!
-   player.x = 200;
-   player.y = 385;
- }  
-};
-
-
-var enemy1 = new Enemy();
-var enemy2 = new Enemy();
-var enemy3 = new Enemy();
-var enemy4 = new Enemy();
-var enemy5 = new Enemy();
-allEnemies.push(enemy1);
-allEnemies.push(enemy2);
-allEnemies.push(enemy3);
-allEnemies.push(enemy4);
-allEnemies.push(enemy5);
-
-
-var Player = function() {
-    InGameObjects.call(this, Player);
-    //start position player
-    this.x = 200; 
-    this.y = 385;
-
-    // player dimensions
+    //x and y position, speed, dimensions
+    this.x = x;
+    this.y = y;
     this.width = 52;
     this.height = 55;
-
-    this.sprite = 'images/char-princess-girl.png';
+    this.speed = Math.floor((Math.random() * 200) + 100);
 }
 
-Player.prototype = Object.create(InGameObjects);
-Player.prototype.constructor = Player;
 
-Player.prototype.update = function() {
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    //if enemy is off map reset position.
+    if (this.x <= 550){
+        this.x += this.speed * dt;
+    } else {
+        this.x = -2;
+    }
+
+    //Collision! 
+    if(player.x >= this.x - 50 && player.x <= this.x + 50){
+        if(player.y >= this.y - 50 && player.y <= this.y + 50){
+            this.reset();
+        }
+    }
+}
+
+
+
+
+
+var Player = function(){
+    this.sprite = 'images/char-princess-girl.png';
+    this.x = 200;
+    this.y = 385;
+}
+
+
+Player.prototype.update = function(){
 };
 
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    };
 
+//Input handler for player
 Player.prototype.handleInput = function(direction) { 
       if (direction === 'left' && this.x > 25) {
         this.x -= 100;
     } if (direction === 'right' && this.x < 400) {
         this.x += 100;
     } if (direction === 'up' && this.y > 0) {
-        this.y -= 82.5;
+        this.y -= 82.5; 
     } if (direction === 'down' && this.y < 400) {
         this.y += 82.5;
     }
 };
 
 
-// Now instantiate your objects.
-var player = new Player();
+
+// Create enemies and player
+
+var enemy1 = new Enemy(-2, 60);
+var enemy2 = new Enemy(-2, 150);
+var enemy3 = new Enemy(-2, 230);
+
+allEnemies.push(enemy1);
+allEnemies.push(enemy2);
+allEnemies.push(enemy3);
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. 
+var player = new Player(); 
+
+
+// listens for key presses and sends the keys to 
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
